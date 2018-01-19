5b8a3987ac39df24f4d3c090390738ebfc78a6fb
marvin <652141046@qq.com>
2018-01-19 22:09:52 +0800

    test for parse

---
+++ test/parse_test.cc
@@ -0,0 +1,24 @@
+#include "../parse.h"
+
+#include <iostream>
+using std::cout;
+using std::endl; 
+
+int main()
+{
+    http_header *hh = alloc_http_header();
+	string http_request(
+        "GET /home/marvin/test HTTP1.1\r\n\
+        Lengh: 8080\r\n\
+        Date: Fri Mon 2018\r\n\
+        \r\n\
+        <html>\n\
+        \thaha\n\
+        </html>"
+    );
+	parse_http_request(http_request, hh);
+	cout << hh->method << " " << hh->url << " " << 
+        hh->version << endl;
+	free_http_header(hh);
+	return 0;
+}
